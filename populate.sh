21111bacb6ebfd175d422d927919fe2536a6ef08
rdoering <rdoering.info@gmail.com>
2024-01-29 02:46:29 +0100

    Refactor Prometheus and StatsD configuration
    Deleted the unnecessary 'statsd_config.js' file and refactored the 'docker-compose.yml' for better services organization. Adjusted the 'prometheus.yml' to update the target configuration while also modifying 'populate.sh' to include different kinds of metrics being sent.

--- populate.sh
+++ populate.sh
@@ -3,11 +3,19 @@
 while :
 do
     GAUGE_EC01=$(( ( RANDOM % 50 )  + 1 ))
+    echo "send [*] ec01: ${GAUGE_EC01}"
+    # with Librato-style tags
+    echo -n "myapp.error#type=critical,host=ec01:${GAUGE_EC01}|g" > /dev/udp/statsd_exporter/9125
+
     GAUGE_EC02=$(( ( RANDOM % 50 )  + 1 ))
-    echo -n "collectd.ec01.nginx.nginx_connections-active:${GAUGE_EC01}|g" > /dev/udp/promstatsd_statsd_1/8125
-    echo -n "collectd.ec02.nginx.nginx_connections-active:${GAUGE_EC02}|g" > /dev/udp/promstatsd_statsd_1/8125
-    echo "Send:"
-    echo " [*] ec01: ${GAUGE_EC01}"
-    echo " [*] ec02: ${GAUGE_EC02}"
+    echo "send [*] ec02: ${GAUGE_EC02}"
+    # with Librato-style tags
+    echo -n "myapp.error#type=critical,host=ec02:${GAUGE_EC02}|g" > /dev/udp/statsd_exporter/9125
+
+    echo increase
+    # Counter (see https://github.com/statsd/statsd/blob/master/docs/metric_types.md)
+    # with Librato-style tags
+    echo -n "myapp.event.total#name=request,host=ec03:1|c" > /dev/udp/statsd_exporter/9125
+
     sleep $1
 done
